95b835388b4fe12e16702a13aa68b98ff9507a40
Shakhriyor <mshax42@mail.ru>
2022-03-30 15:28:15 +0700

    add burger form

---
+++ src/components/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Landing from './Landing';
+import App from './app';
+import NotFound from './NotFound';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Landing}></Route>
+                <Route path='/restaurant/:restaurandId' component={App}></Route>
+                <Route component={NotFound}></Route>
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
